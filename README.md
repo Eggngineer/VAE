78e9dae2d7f9fb8887a416294da8949423073b3e
Eggngineer <yama_shin1999@icloud.com>
2022-06-03 13:19:06 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#  VAE; Visual Auto Encoder
+
+Summary:
+
+I prepared the image reconstruction model with VAE for the purpose of practicing to make some ML models.
+
+Attributes:
+* model
+    * VAE.py
+* test.py
+* train.py
+
---
+++ train.py
@@ -0,0 +1,11 @@
+# Pytorch Reference
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Pytorch IMG Reference
+import torchvision 
+import torchvision.transforms as transforms
+
+# IMG Viewer
+import matplotlib.pyplot as plt
