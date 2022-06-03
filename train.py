86d0bb31eac46dba6bf066bd6bbf46be22b1d42c
Eggngineer <yama_shin1999@icloud.com>
2022-06-03 16:29:54 +0900

    correctly learning is acchieved.

--- train.py
+++ train.py
@@ -1,68 +1,88 @@
 import torch
-from . data . dataloader import MNIST_Loader
-from . model.vae import VAE
+from data . dataloader import MNIST_Loader
+from model.vae import VAE
 import torch.nn.functional as F
+import yaml
+import numpy as np
+from pathlib import Path
+import wandb
+
+BASE_DIR = Path('.')
+WEIGHT_DIR = BASE_DIR
+TRAIN_YML = BASE_DIR
+
+def fix_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+
+def load_yml(yml_path):
+    with open(yml_path) as tyaml:
+        yml = yaml.safe_load(tyaml)
+        return yml
 
 def loss_function(label, predict, mu, log_var):
-  reconstruction_loss = F.binary_cross_entropy(predict, label, reduction='sum')
-  kl_loss = -0.5 * torch.sum(1+ log_var - mu.pow(2) - log_var.exp())
-  vae_loss = reconstruction_loss + kl_loss
-  return vae_loss, reconstruction_loss, kl_loss
+    reconstruction_loss = F.binary_cross_entropy(predict, label, reduction='sum')
+    kl_loss = -0.5 * torch.sum(1+ log_var - mu.pow(2) - log_var.exp())
+    vae_loss = reconstruction_loss + kl_loss
+    return vae_loss, reconstruction_loss, kl_loss
 
 def train(conf):
-  epochs = conf['epochs']
-  image_size = conf['image_size']
-  h_dim = conf['h_dim']
-  z_dim = conf['z_dim']
-  lr = conf['learning_rate']
+    epochs = conf['epochs']
+    image_size = conf['image_size']
+    h_dim = conf['h_dim']
+    z_dim = conf['z_dim']
+    lr = float(conf['learning_rate'])
 
-  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-  model = VAE(image_size, h_dim, z_dim).to(device)
-  optim = torch.optim.Adam(model.parameters(), lr = lr)
+    model = VAE(image_size, h_dim, z_dim).to(device)
+    optim = torch.optim.Adam(model.parameters(), lr = lr)
 
-  losses = []
-  model.train()
-  loader = MNIST_Loader(conf)
+    losses = []
+    model.train()
+    loader = MNIST_Loader(conf)
 
-  for epoch in range(epochs):
-    
-    train_loss = 0
+    for epoch in range(epochs):
 
-    for i, (x, labels) in enumerate(loader):
+        train_loss = 0
 
-      x = x.to(device).view(-1, image_size).to(torch.float32)
-      x_recon, mu, log_var, z = model(x)
-      loss, recon_loss, kl_loss = loss_function(x, x_recon, mu, log_var)
+        for i, (x, labels) in enumerate(loader):
 
-      optim.zero_grad()
-      loss.backward()
-      optim.step()
+            x = x.to(device).view(-1, image_size).to(torch.float32)
+            x_recon, mu, log_var, z = model(x)
+            loss, recon_loss, kl_loss = loss_function(x, x_recon, mu, log_var)
 
-      if (i+1) % 10 == 0:
-        print(f'Epoch: {epoch+1}, loss: {loss: 0.4f}, reconstruction_loss: {recon_loss: 0.4f}, KL loss: {kl_loss: 0.4f}')
-    
-      losses.append(loss)    
-    
-  return losses, model
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            if (i+1) % 10 == 0:
+                wandb.log({'epoch':epoch+1, 'loss':loss})
+                # print(f'Epoch: {epoch+1}, loss: {loss: 0.4f}, reconstruction_loss: {recon_loss: 0.4f}, KL loss: {kl_loss: 0.4f}')
+
+            losses.append(loss)
+        wandb.save("vae.h5")
+
+    return losses, model
 
 
 def main():
-  conf = {
-      'roots' : './data'
-      , 'train' : True
-      , 'downloads' : True
-      , 'batch_size' : 128 
-      , 'shuffle' : True
-      , 'workers' : 0
-      , 'epochs' : 10
-      , 'image_size' : 784 #28*28
-      , 'h_dim' : 32
-      , 'z_dim' : 16
-      , 'learning_rate' : 1e-3
-  }
+    config  = load_yml('yml/train.yml')
+    wandb.init(project=config['project_name'], config=config, name=config['train_name'])
+    conf = wandb.config
 
-  loss, model = train(conf)
+    torch.backends.cudnn.deterministic = True
+    fix_seed(conf['seed'])
+
+    BASE_DIR = Path('.')
+    WEIGHT_DIR = BASE_DIR
+    TRAIN_YML = BASE_DIR
+
+    WEIGHT_DIR = WEIGHT_DIR / conf['weight']
+    TRAIN_YML = TRAIN_YML / conf['yml']
+
+    loss, model = train(conf)
 
 if __name__ == '__main__':
     main()
