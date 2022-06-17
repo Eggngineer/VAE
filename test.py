59e54ab6858a60a4cbf342b5bf5c9e7639b04ca1
Eggngineer <yama_shin1999@icloud.com>
2022-06-18 00:07:05 +0900

    everything is done

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
+from cProfile import label
 import sys
+from tabnanny import check
 sys.dont_write_bytecode = True
 
 import torch
@@ -7,6 +9,10 @@ import wandb
 import numpy as np
 from pathlib import Path
 from model.vae import VAE
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from data . dataloader import MNIST_Loader
 
 def fix_seed(seed):
     torch.manual_seed(seed)
@@ -17,6 +23,17 @@ def load_yml(yml_path):
     with open(yml_path) as tyaml:
         yml = yaml.safe_load(tyaml)
         return yml
+
+
+def show_imgs(data):
+    data = np.asarray(data * 255, dtype=np.uint8)
+    plt.figure("title", figsize=(len(data), 1))
+    for i in range(len(data)):
+        plt.subplot(1, len(data), i+1)
+        plt.axis('off')
+        plt.imshow(data[i].transpose(1, 2, 0), cmap=cm.gray, interpolation='nearest')
+    plt.pause(100)
+    plt.close()
 
 def evals(conf):
     image_size = conf['image_size']
@@ -36,34 +53,62 @@ def evals(conf):
     model.eval()
 
     with torch.no_grad():
-        z = torch.randn(25, z_dim).to(device)
+        loader = MNIST_Loader(conf).__iter__()
+        data, labels = loader.next()
+        show_imgs(data[labels==3])
+        x = data.to(device).view(-1, image_size).to(torch.float32)
+        _,_,z = model.encoder(x)
         out = model.decoder(z)
+        out = out[labels == 3]
 
-    out = out.view(-1,28,28)
+
+        num1 = torch.mean(z[labels == 0],0)
+        num2 = torch.mean(z[labels == 5],0)
+        divn = len(z)-1
+        diff = (num2 - num1) / divn
+        orig_z = torch.zeros(z.shape)
+        for i in range(divn+1):
+            orig_z[:][i] = num1 + i * diff
+        
+        orig_out = model.decoder(orig_z)[:][::11]
+        orig_out = orig_out.view(-1,1,28,28)
+        orig_out = orig_out.cpu().detach().numpy()
+        orig_out = np.array(orig_out)
+        show_imgs(orig_out)
+        
+
+
+
+
+
+
+    out = out.view(-1,1,28,28)
     out = out.cpu().detach().numpy()
+    out = np.array(out)
 
     return z, out
 
 def main():
     config = load_yml('yml/test.yml')
-    wandb.init(project=config['project_name'], config=config, name=config['test_name'])
-    conf = wandb.config
+    # wandb.init(project=config['project_name'], config=config, name=config['test_name'])
+    # config = wandb.config
 
     torch.backends.cudnn.deterministic = True
-    fix_seed(conf['seed'])
+    fix_seed(config['seed'])
 
     BASE_DIR = Path('.')
     WEIGHT_DIR = BASE_DIR
     TEST_YML = BASE_DIR
 
-    WEIGHT_DIR = WEIGHT_DIR / conf['weight']
-    TEST_YML = TEST_YML / conf['yml']
+    WEIGHT_DIR = WEIGHT_DIR / config['weight']
+    TEST_YML = TEST_YML / config['yml']
 
-    z, out = evals(conf=conf)
-    for i in range(len(out)):
-        output_images = wandb.Image(out[i],caption=str(z[i]))
-        wandb.log({'output':output_images})
+    z, out = evals(conf=config)
+    # for i in range(len(out)):
+    #     output_images = wandb.Image(out[i],caption=str(z[i]))
+    #     wandb.log({'output':output_images})
 
+    show_imgs(out)
 
 if __name__ == '__main__':
     main()
